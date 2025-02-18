f32923065b01bc3979affe6ddcdb16142cf316b1
Patricio <patricio.victor.2005@gmail.com>
2025-02-18 13:41:39 +0100

    Corregido

--- python/librerias.py
+++ python/librerias.py
@@ -1,17 +1,28 @@
 import os
-from pathlib import Path
-basepath = '.'
-ruta=input("Introduce ruta de un fichero: ")
+#from pathlib import Path
+#basepath = '.'
+#ruta=input("Introduce ruta de un fichero: ")
 #for entry in os.listdir(basepath):
 #    if not os.path.isfile(os.path.join(basepath, entry)):
 #        print(entry)
 
 
-for entry in os.scandir(basepath):
-    ruta = os.path.basename(ruta)
-    if entry.is_file() and entry.name == ruta:
-        print(ruta.split(basepath)[0])
-        print("encontrado!!")
+#for entry in os.scandir(basepath):
+#    ruta = os.path.basename(ruta)
+#    if entry.is_file() and entry.name == ruta:
+#        print(ruta.split(basepath)[0])
+#        print("encontrado!!")
 
 #docs.python.org/3/library/os.html
 #docs.python.org/3/library/pathlib.html
+
+
+src = input("Introduce la ruta absoluta: ")
+
+array_src = src.split('.')
+dst = ''.join(array.src[:-1])
+
+if os.path.exist(src):
+    os.rename(src, dst)
+else:
+    print("El archivo no existe")
