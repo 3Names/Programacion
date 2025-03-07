02395b18f7a7424b182471c3c649729213c7038d
Patricio <patricio.victor.2005@gmail.com>
2025-03-07 10:49:35 +0100

    Empezamos java, haciendo clases del juego pac-man

---
+++ java/pacman.java
@@ -0,0 +1,19 @@
+public class pacman {
+	private String color = "yellow";i
+	public int comer(){
+		
+	}
+	public pacman(){}
+}
+
+public class fantasma {
+	private String color = "nose";
+}
+
+public class fruta {
+	private String tipo = "fresa";
+}
+
+public class puntos {
+	private int valor = 1;
+}
